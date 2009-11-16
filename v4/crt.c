91c28edc5d0eb35b3d534f37478da9c8bf33cf24
zzero <leidenschaft1@gmail.com>
2009-11-16 18:23:07 -0500

    version4

---
+++ v4/crt.c
@@ -0,0 +1,62 @@
+/* CRT */
+
+#include <stdio.h>			//standard in/out
+#include <signal.h>			//signals defined
+#include <sys/mman.h>		//memory map
+
+#include "global.h"			//our global parameters
+
+void die()	//kills itself
+{
+	exit(0);
+}
+
+int main(int rtx_pid, int crt_sm_fid)	//get rtx_pid, fid from RTX during fork
+{
+	sigset(SIGINT, die);//when parent send SIGINT, abort
+
+	int index = 0;	//counter for num of char in crt_sm
+	char c;
+
+	//memory map
+	mmap_ptr = mmap
+	(
+		(caddr_t) 0,				//memory location; 0 lets OS choose
+		SMSIZE,						//bytes to map
+		PROT_READ | PROT_WRITE,		//read and write permissions
+		MAP_SHARED,					//accessible by another process
+		crt_sm_fid,					//which file is associated with mmap
+		(off_t) 0					//offset in page frame	//off_t from types.h
+	);	
+
+	if (mmap_ptr == MAP_FAILED)		//if memory map fail, abort KB
+	{
+		printf("Child memory map has failed, CRT is aborting!\n");
+		die();
+	}
+
+	crt_sm_ptr = (crt_sm *) mmap_ptr;	//sm pointer to the memory mapped
+	crt_sm_ptr->status = 0;				//0: ready for CRT
+										//1: ready for RTX
+
+	while(1)	//until killed by parent
+	{
+		while(kb_sm_ptr->status == 1)
+			usleep(SLEEP);
+
+		while(crt_sm_ptr->data[index] != '\0')
+		{
+			c = data[index];	//get the char to be displayed
+			printf ("%c", c);	//print to screen the char
+			index+=1;
+		}
+
+		printf("\n");	//newline
+
+		crt_sm_ptr->status = 1;		//ready for rtx to put more char
+		kill(rtx_pid, SIGUSR2);			//send a signal to parent			
+		crt_sm_ptr->index = 0;		//reset index
+
+		//now go back to waiting for status bit to change
+	}
+}
---
+++ v4/keyboard.c
@@ -0,0 +1,73 @@
+/* KB */
+
+#include <stdio.h>			//standard in/out
+#include <signal.h>			//signals defined
+#include <sys/mman.h>		//memory map
+//#include <sys/wait.h>		//wait() function. don't think we need this
+//#include <fcntl.h>		//open() function. don't think we need this
+//#include <sys/types.h>	//might need it for caddr_t struct
+#include "global.h"			//our global parameters
+
+void die()	//kills itself
+{
+	exit(0);	//where is this function from?
+}
+
+int main(int rtx_pid, int kb_sm_fid)	//get rtx_pid, fid from RTX during fork
+{
+	//variables
+	caddr_t mmap_ptr;	//ptr to memory mapped	//where is this struc from?
+	kb_sm * kb_sm_ptr;	//ptr to shared memory
+	int index = 0;		//index of char data in shared memory
+	char c;				//char inputted
+
+	sigset(SIGINT,die);	//register SIGINT to run die
+	
+	//memory map
+	mmap_ptr = mmap
+	(
+		(caddr_t) 0,			//memory location; 0 lets OS choose
+		SMSIZE,						//bytes to map
+		PROT_READ | PROT_WRITE,		//read and write permissions
+		MAP_SHARED,					//accessible by another process
+		kb_sm_fid,					//which file is associated with mmap
+		(off_t) 0					//offset in page frame	//off_t from types.h
+	);					
+
+	if (mmap_ptr == MAP_FAILED)		//if memory map fail, abort KB
+	{
+		printf("Child memory map has failed, KB is aborting!\n");
+		die();
+	}
+
+	kb_sm_ptr = (kb_sm *) mmap_ptr;	//sm pointer to the memory mapped
+	kb_sm_ptr->status = 0;			//0: ready for KB
+									//1: ready for RTX
+
+	while(1)	//until killed by parent
+	{
+		c = getchar();						//wait until input (blocked)
+
+		if ( c != '\n' )					//not carriage return
+		{
+			if (index < KB_MAXCHAR - 1)		//last one is saved for the null char
+			{
+				kb_sm_ptr->data[index] = c;	//put input char into sm
+				index+=1;					//increment index
+			}
+		}
+
+		else	//if carriage return or buffer full
+		{
+			kb_sm_ptr->data[index] = '\0';	//last char is null
+			kb_sm_ptr->status = 1;			//ready for rtx to take away char
+			kill(rtx_pid, SIGUSR1);				//send a signal to parent
+			index = 0;						//restart index
+
+			while(kb_sm_ptr->status == 1)
+				usleep(SLEEP);
+			
+			//now go back to waiting for user input char
+		}
+	}
+}
