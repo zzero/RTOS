a215389d51952b813d42877694bf0534ca4c15e1
zzero <leidenschaft1@gmail.com>
2009-11-22 16:56:13 -0500

    chaning RTX.c

--- RTX.c
+++ RTX.c
@@ -1,31 +1,38 @@
-//hyesun edit2
-#include "include/RTX.h"
+
+#include "RTX.h"
 
 int main(){
 	
-	Initialization();
-	
-	MsgEnv *temp;
-	temp = (MsgEnv*)malloc(sizeof(MsgEnv));
-	
-	temp->env_id=0;
-	temp->next=NULL;
-	temp->sender_id=0;
-	temp->dest_id = 0;
-	temp->type = 0;
-	temp->num_clock_ticks= 0;
-	
-	
-	//~ int env_id
-	//~ struct MsgEnv *next; //used when env is on a queue
-	//~ int sender_id;
-	//~ int dest_id;
-	//~ int type;
-	//~ int num_clock_ticks;
-	//~ char text_area[TEXT_AREA_SIZE];
-
-	temp = K_request_process_status(temp);
-	//~ printf("%s",temp->text_area);
 	return 0;
 }
 
+void atomic(bool status);
+{
+	static sigset_t oldmask;
+	sigset_t newmask;
+	if (on) 
+	{
+		current_process->atomic_count = current_process->atomic_count + 1;
+		if (current_process->atomic_count == 1) 
+		{
+			sigemptyset(&newmask);
+			sigaddset(&newmask, 14); //the alarm signal
+			sigaddset(&newmask, 2); // the CNTRL-C
+			sigaddset(&newmask, SIGUSR1);
+			sigaddset(&newmask, SIGUSR2);
+			sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+		}
+
+	} 
+	else 
+	{
+	current_process->atomic_count = current_process->atomic_count - 1;
+		if (current_process->atomic_count == 0) 
+		{
+			//unblock the signals
+			sigprocmask(SIG_SETMASK, &oldmask, NULL);
+		}
+	}
+
+
+}
