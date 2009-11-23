b2247b1b368ff2242b8b72dd253b3ec105207078
zzero <leidenschaft1@gmail.com>
2009-11-22 20:46:54 -0500

    putting branavan's changes

--- CCI.c
+++ CCI.c
@@ -1,14 +1,185 @@
+
+/*Command Console Interface*/
 void CCI()
 {
+     
+     /* Output CCI: to display wait for acknowledge, send msg 
+     to kb_i_proc to return latest keyboard input*/  
+     
      MsgEnv* CCI_env;    
-     CCI_env->msg_txt = "CCI:"
-	 send_console_chars(CCI_env)
-	 CCI_env =  receive_message()
+     CCI_env.txt_area = "CCI:";
+	 send_console_chars(CCI_env);
+	 
 	
-     if (CCI_env.msg_type == DISPLAY_ACKNOWLEDGE)
+     while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
 	 {  
-       get_console_chars(CCI_env)
-	   CCI_env = receive_message()
+           CCI_env =  receive_message();
      }
 	 
- }
+     get_console_chars(CCI_env);
+     
+     while (CCI_eng.msg_type != CONSOLE_INPUT)
+     {
+	       CCI_env = receive_message();
+     }
+    
+     /*Store text of CCI_env into character string, convert to lowercase*/
+     char CCI_store[10]={\0};
+     CCI_store = CCI_env.txt_area;
+     stolower(CCI_store);
+     
+     /*send message envelope to User Process A*/
+     if(CCI_store == "s")
+     {
+        CCI_env.txt_area = CCI_store; 
+        send_message(1, CCI_env); // 1 -> refers to USERPROC_A
+        
+        send_console_chars(CCI_env);
+        while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
+	     {  
+           CCI_env =  receive_message();
+         }
+       
+       break;
+       
+     }
+     
+     /*Display Process Status of all processes*/
+     else if(CCI_store == "ps")
+     {
+        CCI_env.txt_area = CCI_store; 
+        request_process_status(CCI_env);
+        
+        send_console_chars(CCI_env);
+        while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
+	     {  
+           CCI_env =  receive_message();
+         }      
+     
+        break; 
+     }
+     
+     /*Sets CCI wall clock to desired format*/
+     else if(CCI_store == "c hh:mm:ss")
+     {
+         CCI_env.txt_area = CCI_store;
+         
+         send_console_chars(CCI_env);
+         while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
+	     {  
+           CCI_env =  receive_message();
+         } 
+             
+         MsgEnv* CCI_clock;    
+         CCI_clock.msg_type = SET_CLOCK;  //declare SET_CLOCK as global
+         CCI_clock.txt_area = CCI_store;
+         send_message(CLOCK_pid, CCI_clock); //define CLOCK_pid as global
+         
+         break;
+      
+     }
+     
+     /*Display wall clock*/
+     else if(CCI_store =="cd")
+     {
+         MsgEnv* CCI_clock;    
+         CCI_clock.msg_type = CLOCK_ON;  //declare SET_CLOCK as global
+         CCI_clock.txt_area = CCI_store;
+         send_message(CLOCK_pid, CCI_clock); //define CLOCK_pid as global
+         
+         break;
+     
+     }
+     
+     /*Clear wall clock*/
+     else if(CCI_store == "ct")
+     {
+         CCI_env.txt_area = " ";
+         
+        send_console_chars(CCI_env);
+        while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
+	     {  
+           CCI_env =  receive_message();
+         } 
+        
+       break;       
+     }
+     
+     /*Display contents of send and recieve trace buffers*/
+     else if(CCI_store == "b")
+     {
+         CCI_env.txt_area = CCI_store;
+         
+         get_trace_buffers(CCI_env);
+         
+         send_console_chars(CCI_env);
+         while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
+	     {  
+           CCI_env =  receive_message();
+         } 
+         
+      break;   
+     }
+     
+     /*Terminate RTX*/
+     else if(CCI_store == "t")
+     {
+         CCI_env.txt_area = CCI_store;
+         
+         send_console_chars(CCI_env);
+         while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
+	     {  
+           CCI_env =  receive_message();
+         } 
+         
+         sig_handler(SIGINT);
+                     
+     }
+     
+     /*Change priority of a specified process*/
+     else if(CCI_store[0] == "n")
+     {
+         CCI_env.txt_area = CCI_store;
+         int priority, id; 
+         
+         for(int i = 0; i<10; i++)
+         {
+            if(i == 1 && CCI_store[i] == " ")
+                id = CCI_store[i+1];    
+            
+            else if(i == 3 && CCI_store[i] == " ")
+                 priority = CCI_store[i+1];
+             
+         }      
+         
+         change_priority(priority, id);
+         
+         send_console_chars(CCI_env);
+         while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
+	     {  
+           CCI_env =  receive_message();
+         } 
+     
+     }
+     
+     /*User enters blank space*/
+     else if(CCI_store == " ")
+     {
+         CCI_env.txt_area = CCI_store;
+         
+         send_console_chars(CCI_env);
+         while (CCI_env.msg_type != DISPLAY_ACKNOWLEDGEMENT)
+	     {  
+           CCI_env =  receive_message();
+         }
+         
+         break;
+      } 
+     
+          
+}
+         
+      
+     
+ 
+ 
